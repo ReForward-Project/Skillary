af763a41409c9c03ebab41a1cc387c9fbcf3bbf8
seonghun120614 <seonghun120614@gmail.com>
2026-01-08 17:14:46 +0900

    chore: dev 환경 세팅

---
+++ spring-skillary-back/src/main/java/com/example/springskillaryback/SpringSkillaryBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.springskillaryback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSkillaryBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSkillaryBackApplication.class, args);
+	}
+
+}
---
+++ spring-skillary-back/src/test/java/com/example/springskillaryback/SpringSkillaryBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springskillaryback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSkillaryBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
